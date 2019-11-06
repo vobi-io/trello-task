b5610c8c77cd4656a63bfbe7a9b25b46b2a6e18d
saba-coding <saba.chikhina@gmail.com>
2019-11-06 14:27:27 +0400

    refactor: index.js

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ module.exports = ({ reqBody, keyWord }) => {
 
   const service = require('./service')(reqBody);
 
-  exec('git rev-parse `git branch -r --sort=committerdate | tail -1`', (err, stdout) => {
-    if (err) return;
+  exec('git rev-parse `git branch -r --sort=committerdate | tail -1`', (err1, remoteBranch) => {
+    if (err1) return;
+    exec('git rev-parse HEAD', (err2, localBranch) => {
+      if (err2) return;
 
-    exec(`git log -u -1 ${stdout}`, (error, out) => {
-      if (error) return;
+      exec(`git diff ${remoteBranch} ${localBranch}`, (error, out) => {
+        if (error) return;
 
-      const latest = out.split('\n').filter((diff) => diff.indexOf(keyWord) !== -1
+        const latest = out.split('\n').filter((diff) => diff.indexOf(keyWord) !== -1
       && diff.indexOf('+') !== -1 && diff.indexOf('+') === 0);
-      latest.forEach((task) => {
-        const description = task.split(`${keyWord} `).reverse()[0];
-        service.createCard({ name: description });
+        latest.forEach((task) => {
+          const description = task.split(`${keyWord} `).reverse()[0];
+          service.createCard({ name: description });
+        });
       });
     });
   });
